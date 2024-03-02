0f95a62922bde86d4b00ee530d9ffbaf28f38631
Lara Souza <laragabriellysouzabatista@gmail.com>
2024-03-01 23:13:14 -0300

    added slick carousdl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,15 @@ import { HeaderComponent } from "./components/header/header.component";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServicesOfferedComponent } from "./components/services-offered/services-offered.component";
 import { LashesSectionComponent } from "./components/lashes-section/lashes-section.component";
+import { CursosComponent } from "./components/cursos/cursos.component";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, NgbModule, ServicesOfferedComponent, LashesSectionComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, NgbModule, ServicesOfferedComponent, LashesSectionComponent, CursosComponent, SlickCarouselModule]
 })
 export class AppComponent {
   title = 'Mayara-studio';
